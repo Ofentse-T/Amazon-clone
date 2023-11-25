1d6e93720835de41b7c13ca5f42923623eaba42c
Ofentse-T <122441914+Ofentse-T@users.noreply.github.com>
2023-11-25 13:36:27 +0200

    Installed node modules, router and added home and products page

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import { Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Amazon Clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/components/Products.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Products = () => {
+  return (
+    <div>
+      <h1>Product list</h1>
+    </div>
+  );
+};
+
+export default Products;
