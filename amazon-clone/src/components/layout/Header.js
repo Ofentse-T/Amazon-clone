698336ea0e8fe8899dc07d9f7fad7bf2c0ab8782
Ofentse-T <122441914+Ofentse-T@users.noreply.github.com>
2023-11-26 17:20:27 +0200

    Uninstalled icons

--- amazon-clone/src/components/layout/Header.js
+++ amazon-clone/src/components/layout/Header.js
@@ -1,24 +1,42 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import { Link, NavLink } from "react-router-dom";
 
-export const Header = () => {
+const Header = () => {
   return (
     <header className="header">
-      <nav>
-        <ul>
-          <li>
-            <NavLink activeClassName="active" to="/home">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" to="/products">
-              Products
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <img
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="Amazon logo"
+      />
+      <div className="header_search">
+        <input className="header_input" type="text">
+          <SearchIcon className="search_icon" />
+        </input>
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_option1">Hello Guest</span>
+          <span className="header_option2">Sign In</span>
+        </div>
+
+        <div className="header_option">
+          <span className="header_option1">Returns</span>
+          <span className="header_option2">& Orders</span>
+        </div>
+
+        <div className="header_option">
+          <span className="header_option1">Your</span>
+          <span className="header_option2">Prime</span>
+        </div>
+
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header_option2 header_basketCount">0</span>
+        </div>
+      </div>
     </header>
   );
 };
